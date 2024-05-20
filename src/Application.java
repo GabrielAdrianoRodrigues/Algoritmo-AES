9786637c21dffd595a2decd21a4954061d4f184b
Gabriel Adriano Rodrigues <gabrieladriano@furb.br>
2024-05-19 22:28:59 -0300

    finalizando logica da classe do algoritmo AES

--- src/Application.java
+++ src/Application.java
@@ -3,20 +3,28 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
 
-
-
 public class Application {
     public static final JFileChooser c = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         // c.showOpenDialog(null);
-        // System.out.println("Digite o nome do arquivo destino:");
+        // System.out.println(Digite o nome do arquivo destino:);
         // Paths.get(sc.next());   
-        // System.out.println("Digite a chave de criptografia");
+        // System.out.println(Digite a chave de criptografia);
         // c.getSelectedFile().toPath()
 
-        new AESAlgorithm().lastRoundKey("20,1,94,33,199,0,48,9,31,94,112,40,59,30,100,248".split(","));
-        sc.close();
+        AESAlgorithm.generateRoundsKeys("65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80").forEach(x -> {
+            for (int i = 0; i < x.length; i++) {
+                for (int j = 0; j < x[i].length; j++) {
+                    System.out.print(Integer.toHexString(x[i][j])+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        });
+        //sc.close();
     }
+
+
 }
